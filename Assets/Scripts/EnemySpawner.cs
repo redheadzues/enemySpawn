af56f1d04de6e5da05d43e7e599ada00b283ba45
redheadzues <48310838+redheadzues@users.noreply.github.com>
2022-06-20 10:17:16 +0300

    thrid commit

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _spawnTime;
+
+    private Transform[] _spawnPoints;
+    private float _runningTime;
+
+    
+    private void Start()
+    {
+        _spawnPoints = GetComponentsInChildren<Transform>();
+        StartCoroutine(SpawnEnemy());
+    }
+
+
+
+    private IEnumerator SpawnEnemy()
+    {
+        while(_spawnTime > 0)
+        {
+            _runningTime += Time.deltaTime;
+
+            if (_runningTime > _spawnTime)
+            {
+                Transform spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+
+                Instantiate(_enemy, spawnPoint.position, Quaternion.identity);
+
+                _runningTime = 0;
+            }
+
+            yield return null;
+        }
+    }
+}
